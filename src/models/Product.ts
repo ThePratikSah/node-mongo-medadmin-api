0e77c9d3c524f02352319dc5305e5c3713d98801
Pratik Sah <shivam3448@gmail.com>
2024-01-10 03:10:02 +0530

    Added Product schema

---
+++ src/models/Product.ts
@@ -0,0 +1,62 @@
+import { Schema, Types, model } from "mongoose";
+
+export interface IProduct {
+  _id?: Types.ObjectId;
+  name: string;
+  description: string;
+  price: number;
+  selling_price: number;
+  quantity?: number;
+  prescription_required: boolean;
+  expiry_date: Date;
+  manufacturer: string;
+  category: string;
+  images: string[];
+}
+
+const productSchema = new Schema<IProduct>({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  description: {
+    type: String,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  selling_price: {
+    type: Number,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: false,
+    default: 0,
+  },
+  prescription_required: {
+    type: Boolean,
+    required: true,
+  },
+  expiry_date: {
+    type: Date,
+    required: true,
+  },
+  manufacturer: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+  },
+  images: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+});
+
+const Product = model<IProduct>("Product", productSchema);
