c8c0ac3d6079515d86ad058d791727cb7c74a9e7
Pratik Sah <shivam3448@gmail.com>
2024-01-09 19:47:09 +0530

    Added middleware validation

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,19 +1,27 @@
 import { NextFunction, Request, Response } from "express";
+import { string, email, minLength, object, safeParse, Input } from "valibot";
 
-export interface RequestBody {
-  email: string;
-  password: string;
-}
+const AuthPayloadBodySchema = object({
+  email: string("Your email must be a string.", [
+    minLength(1, "Please enter your email."),
+    email("The email address is badly formatted."),
+  ]),
+  password: string("Your password must be a string.", [
+    minLength(8, "Your password must have 8 characters or more."),
+  ]),
+});
+
+export type RequestBody = Input<typeof AuthPayloadBodySchema>;
 
 export async function checkAuthData(
   req: Request<{}, {}, RequestBody>,
   res: Response,
   next: NextFunction
 ) {
-  const { email, password } = req.body;
+  const result = safeParse(AuthPayloadBodySchema, req.body);
 
-  if (!email || !password) {
-    return res.status(400).json({ msg: "Email and password are required" });
+  if (!result.success) {
+    return res.status(400).json({ msg: "Invalid data", errors: result.issues });
   }
   return next();
 }
