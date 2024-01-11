849b5e549c431f34262c9ffa67b4ed124958a11d
Pratik Sah <shivam3448@gmail.com>
2024-01-11 18:08:07 +0530

    Added category helper methods

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -28,3 +28,11 @@ const CategorySchema = new Schema<Category>(
 );
 
 const Category = model<Category>("Category", CategorySchema);
+
+export const createCategory = (category: Category) =>
+  new Category(category).save();
+export const getAllCategories = () => Category.find();
+export const getCategoryById = (id: string) => Category.findById(id);
+export const deleteCategory = (id: string) => Category.findByIdAndDelete(id);
+export const updateCategory = (id: string, category: Category) =>
+  Category.findByIdAndUpdate(id, category);
