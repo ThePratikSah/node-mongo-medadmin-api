5af39b64f9681690a241807c9ce9e2d2b8e11637
Pratik Sah <shivam3448@gmail.com>
2024-01-20 23:56:37 +0530

    Added middleware authorization for category

--- src/routes/category.route.ts
+++ src/routes/category.route.ts
@@ -10,16 +10,27 @@ import {
   checkCategoryParams,
   checkCategoryPayloadData,
 } from "../middleware/category.middleware";
+import { restrictTo } from "../middleware/auth.middleware";
 
 export const router = Router();
 
-router.post("/", checkCategoryPayloadData, createCategoryController);
+router.post(
+  "/",
+  checkCategoryPayloadData,
+  restrictTo(["admin", "manager"]),
+  createCategoryController
+);
 router.get("/", getAllCategoryController);
 router.get("/:categoryId", getCategoryByIdController);
 router.put(
   "/:categoryId",
   checkCategoryPayloadData,
   checkCategoryParams,
+  restrictTo(["admin", "manager"]),
   updateCategoryController
 );
-router.delete("/:categoryId", deleteCategoryController);
+router.delete(
+  "/:categoryId",
+  restrictTo(["admin", "manager"]),
+  deleteCategoryController
+);
